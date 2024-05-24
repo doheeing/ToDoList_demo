b0e589ee9490144a375135eb21c6229e92cd3fcb
doheeing <doheeehan@naver.com>
2024-05-24 15:27:32 +0900

    BE 배포 완료

--- todo_BE/app.js
+++ todo_BE/app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const indexRouter = require("./routes/index")
-
+const cors = require("cors");
+const indexRouter = require("./routes/index");
+require("dotenv").config();
 const app = express();
-app.use(bodyParser.json());
-app.use('/api', indexRouter)
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api", indexRouter);
+
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
@@ -18,6 +22,6 @@ mongoose
     console.log("DB connection fail");
   });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server on 5000");
 });
